1bc0208331b19a8a947af9facc5e8d1801638706
Ayushijaiswa <ayushijaiswal0209@gmail.com>
2024-07-21 14:20:09 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useState } from 'react'
+export default function App(){
+  const [formData,setData]=useState({
+    email:"",password:"",confirmpassword:"",joined:true
+  })
+ function handle(event){
+   const {name,type,checked,value}=event.target;
+   setData(prev=>({
+    ...prev,
+    [name]:type=="checkbox"?checked:value
 
-function App() {
-  const [count, setCount] = useState(0)
+   }))
+ }
+ function handlesubmit(event){
+  event.preventDefault
+  if(formData.password==formData.confirmpassword && formData.joined){
+    alert("submited successfully")
+  }
+  else alert("please fill the form correctly")
+  if(!formData.joined){
+    alert("signing up require checking the box")
+  }
+ }
+
+
+
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='form-container'>
+      <form 
+      className='form'
+      onSubmit={handlesubmit}
+      >
+        <input 
+        type="text"
+         name="email" 
+         id="email" 
+         placeholder='email'
+         className='input'
+         onChange={handle}
+         value={formData.email}
+        />
+        <input
+         type="password" 
+         name="password" 
+         placeholder='password'
+         id='password'
+         className='input'
+         onChange={handle}
+         value={formData.password}
+          />
+         <input 
+         type="password" 
+         name="confirmpassword"
+         placeholder='confirm passwaord'
+         id='cnf'
+         className='input'
+         onChange={handle}
+         value={formData.confirmpassword}
+         />
+         <div className='letter'>
+          <input type="checkbox" 
+          name="joined" 
+          onChange={handle}
+          checked={formData.joined}
+          id="okay" />
+          <label htmlFor="okay">I want to join the newsletter</label>
+          </div>
+         <button className='button'> Sign up</button>
+      </form>
+    </div>
   )
 }
-
-export default App
